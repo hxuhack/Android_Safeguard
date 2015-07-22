7eb806a9d7f8c286cc629bf054179ac8066f97c0
hxuhack <china.xuhui@gmail.com>
2015-07-22 15:45:38 +0800

    update

---
+++ obj/local/armeabi-v7a/objs/nvo/main.o.d
@@ -0,0 +1,228 @@
+./obj/local/armeabi-v7a/objs/nvo/main.o: main.c sha1.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/stdio.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/cdefs.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/cdefs_elf.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/android/api-level.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/types.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/stdint.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/machine/wchar_limits.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/types.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/types.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/types.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/int-ll64.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/bitsperlong.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/bitsperlong.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/posix_types.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/stddef.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/compiler.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/posix_types.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/posix_types.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/sysmacros.h \
+  sec.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/socket.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/socket.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/fcntl.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/fcntl.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/socket.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/socket.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/sockios.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/sockios.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/sockios.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/uio.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/netinet/in.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/endian.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/endian.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/machine/endian.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/netinet/in6.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/in6.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/libc-compat.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/in.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/byteorder.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/byteorder/little_endian.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/swab.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/swab.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/ipv6.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/arpa/inet.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/netdb.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/unistd.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/select.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/time.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/time.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/signal.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/errno.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/errno.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/errno.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/errno.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/errno-base.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/limits.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/limits.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/limits.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/syslimits.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/string.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/malloc.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/xlocale.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/sigcontext.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/signal.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/signal.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/signal-defs.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/siginfo.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/siginfo.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/ucontext.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/user.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/sysconf.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/pathconf.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/jni.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/android/log.h \
+  /opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/assert.h
+
+sha1.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/stdio.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/cdefs.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/android/api-level.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/types.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/stdint.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/machine/wchar_limits.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/types.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/types.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/types.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/int-ll64.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/bitsperlong.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/bitsperlong.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/posix_types.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/stddef.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/compiler.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/posix_types.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/posix_types.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/sysmacros.h:
+
+sec.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/socket.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/socket.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/fcntl.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/socket.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/socket.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/sockios.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/sockios.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/sockios.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/uio.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/netinet/in.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/endian.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/endian.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/machine/endian.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/netinet/in6.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/in6.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/libc-compat.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/in.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/byteorder.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/byteorder/little_endian.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/swab.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/swab.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/ipv6.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/arpa/inet.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/netdb.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/unistd.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/select.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/time.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/time.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/signal.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/errno.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/errno.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/errno.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/errno.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/limits.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/limits.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/limits.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/syslimits.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/string.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/malloc.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/xlocale.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/sigcontext.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/linux/signal.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/signal.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/signal-defs.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm/siginfo.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/ucontext.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/user.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/sys/sysconf.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/pathconf.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/jni.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/android/log.h:
+
+/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/assert.h:
