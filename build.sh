d9eef2e1d7dea593cf002fa80e28d8c586e36844
hxuhack <china.xuhui@gmail.com>
2015-10-05 17:24:03 +0800

    update

--- build.sh
+++ build.sh
@@ -4,12 +4,11 @@
 
 #rm *.txt
 
-#nvgen ~/gadget/sha1.cpp -L=0 --
+#nvgen ~/gadget/sha1.cpp -L=9 --
 #nvgen ~/gadget/sha1.cpp -L=1 --
-#nvgen ~/gadget/sha1.cpp -L=2 --
-#nvgen ~/gadget/sec.cpp -L=2 --
-#nvgen ~/gadget/utils.cpp -L=2 --
-#nvgen ~/gadget/nvobridge.cpp -L=9 --
+#nvgen ~/gadget/sec.cpp -L=1 --
+#nvgen ~/gadget/utils.cpp -L=1 --
+#nvgen ~/gadget/nvobridge.cpp -L=0 --
 
 ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android_exec.mk NDK_APPLICATION_MK=./Application.mk 
 ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android_so.mk NDK_APPLICATION_MK=./Application.mk 
