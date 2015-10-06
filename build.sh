efca794c755852a02dbdf30a5631c01387166b58
hxuhack <china.xuhui@gmail.com>
2015-10-06 20:09:56 +0800

    update

--- build.sh
+++ build.sh
@@ -4,6 +4,18 @@
 
 #rm *.txt
 
+cp original/sec_utils.h sec_utils.h
+cp original/sec_utils.cpp sec_utils.cpp
+cp original/sec_basis.h sec_basis.h
+cp original/sec_basis.cpp sec_basis.cpp
+cp original/sha1.cpp sha1.cpp
+cp original/sha1.h sha1.h
+cp original/utils.cpp utils.cpp
+cp original/utils.h utils.h
+cp original/obf_bridge.cpp obf_bridge.cpp
+cp original/obf_bridge.h obf_bridge.h
+cp original/main.cpp main.cpp
+
 if [ -n "$1" ];
 then
     nvgen ~/gadget/sha1.cpp -L=9 --
@@ -11,7 +23,7 @@ then
     nvgen ~/gadget/sec_basis.cpp -L=1 --
     nvgen ~/gadget/sec_utils.cpp -L=1 --
     nvgen ~/gadget/utils.cpp -L=1 --
-    nvgen ~/gadget/main.cpp -L=0 --
+    nvgen ~/gadget/obf_bridge.cpp -L=0 --
 fi
 
 ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android_exec.mk NDK_APPLICATION_MK=./Application.mk 
--- original/main.cpp
+++ original/main.cpp
@@ -1,96 +1,37 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <netdb.h>
-#include <unistd.h>
+#include "obf_bridge.h"
 #include "/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/jni.h"
-#include "/opt/android-ndk-r10e/platforms/android-21/arch-arm/usr/include/android/log.h"
-#include <assert.h>
-#include <sys/ptrace.h>
-#include <resolv.h>
-#include "sec_utils.h"
-#include "sha1.h"
 
 extern "C" {
-static int socketid;
 
 JNIEXPORT jint JNICALL Java_edu_nvo_lib_jni_traceme( JNIEnv *env, jobject thiz){
-    int result = ptrace(PTRACE_TRACEME,0 ,0 ,0);
-    if(result != 0){
-        exit(0);
-    }
-    return result;
+    return jni_traceme();
 }
 
 JNIEXPORT jint JNICALL Java_edu_nvo_lib_jni_chkroot( JNIEnv *env, jobject thiz){
-    int result = check_root();
-    return result;
+    return jni_check_root();
 }
 
 JNIEXPORT jint JNICALL Java_edu_nvo_lib_jni_chkemulator( JNIEnv *env, jobject thiz){
-    int result = check_emulator();
-    return result;
+    return check_emulator();
 }
 
 JNIEXPORT jint JNICALL Java_edu_nvo_lib_jni_socketconnect(
                        JNIEnv *env, jobject thiz,
                        jstring hostname, 
 		       jint port){
-    struct sockaddr_in pin;
-    struct hostent *nlp_host;
 
     const char *str_hostname = env->GetStringUTFChars(hostname, 0);
-    //res_init();
-
-    if ((nlp_host=gethostbyname(str_hostname))==NULL){
-	__android_log_print(ANDROID_LOG_INFO, "NVO", "Resolve Error:%s", str_hostname);
-	return -1;
-    }
- 
- 
-    bzero(&pin,sizeof(pin));
-    pin.sin_family=AF_INET;                 
-    pin.sin_addr.s_addr=htonl(INADDR_ANY);  
-    pin.sin_addr.s_addr=((struct in_addr *)(nlp_host->h_addr))->s_addr;
-    pin.sin_port=htons(port);
- 
-    socketid=socket(AF_INET,SOCK_STREAM,0);
- 
-    if (connect(socketid,(struct sockaddr*)&pin,sizeof(pin))==-1){
-        __android_log_print(ANDROID_LOG_INFO, "NVO", "Connect Error!");
-	return -1;
-    }
-    return socketid;
-    
+    return jni_socket_connect(str_hostname, port);
 }
 
 
 JNIEXPORT jint JNICALL Java_edu_nvo_lib_jni_secrequest(
                         JNIEnv *env, jobject thiz,
-                        jstring msg){
+                        jstring jmsg,
+			jint socketid){
 
-    const char *msg = env->GetStringUTFChars(msg, 0);
-
-    uint8_t digest[SHA1HashSize];
-    string memchk[MSG_SIZE];
-    string msg_to_server;
-    int i;
-
-    SHA1(msg, digest);
-
-    int numof_vialib = check_proc(memchk);
-    
-    msg_to_server = (char *) digest;
-    for (i=0; i<numof_vialib; i++ ){
-        msg_to_server = msg_to_server + memchk[i];
-    }
-
-    //__android_log_print(ANDROID_LOG_INFO, "NVO", "msg_to_server: %s", msg_to_server);
-    send(socketid, msg_to_server.c_str(), msg_to_server.length(), 0);
-
-    return 0;
+    const char *msg = env->GetStringUTFChars(jmsg, 0);
+    jni_socket_msg(socketid, msg);
 }
 
 int main(int argc, char *argv[]){
@@ -99,11 +40,11 @@ int main(int argc, char *argv[]){
         switch(opt){
 	    case 'r':
 	        //check root
-                check_root();
+                jni_check_root();
 	        break;
             case 'e':
                 //detect emulator
-                check_emulator();
+                jni_check_emulator();
 	        break;
             case 't':
                 //traceme
